7ec49f6392cdbadb7ebcae737cdbdb39c949586d
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-04-04 21:54:43 +0200

    initial

---
+++ main.c
@@ -0,0 +1,11 @@
+// Pomocou #include pripojte do 
+// programu hlavickovy subor 'config.h'.
+#include "config.h"
+
+int main(int argc, char *argv[]) {
+	// TODO:
+	// Tu vypracujte zadanie, pouzivajte vlastne pomocne funkcie.
+	//    - pouzivajte makra/struktury z 'config.h'
+	//    - pouzite databazu restauracii, ktora je definovana v subore 'data.c'
+	return 0;
+}
