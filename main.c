cea39f0c138e6d25dfae57dca5219ab1aabb4a6f
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-04-08 16:34:02 +0200

    isNumber, checks if string is a number

--- main.c
+++ main.c
@@ -9,10 +9,23 @@
 #define TRUE 1
 #define FALSE 0
 #define POS_DELIM ","
+int isNumber(char *string);
 int main(int argc, char *argv[]) {
 	// TODO:
 	// Tu vypracujte zadanie, pouzivajte vlastne pomocne funkcie.
 	//    - pouzivajte makra/struktury z 'config.h'
 	//    - pouzite databazu restauracii, ktora je definovana v subore 'data.c'
 	return 0;
+}
+//return 1 ak je string cislo, inac 0
+int isNumber(char *string)
+{
+	for (int i = 0; i < strlen(string); i++)
+	{
+		if(isdigit(string[i]) == FALSE)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
