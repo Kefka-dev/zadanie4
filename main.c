fe9f32c344ca4425e3f273261ca0c9ec3aef0889
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-04-08 16:41:31 +0200

    extractPosition, now handle E1 case

--- main.c
+++ main.c
@@ -9,12 +9,33 @@
 #define TRUE 1
 #define FALSE 0
 #define POS_DELIM ","
+int extractPosition(char* positionString, int* posX, int* posY);
 int isNumber(char *string);
 int main(int argc, char *argv[]) {
 	// TODO:
 	// Tu vypracujte zadanie, pouzivajte vlastne pomocne funkcie.
 	//    - pouzivajte makra/struktury z 'config.h'
 	//    - pouzite databazu restauracii, ktora je definovana v subore 'data.c'
+int extractPosition(char* positionString, int* posX, int* posY){
+	// sscanf(positionString, "%d,%d", posX, posY);
+	char *token1, *token2;
+	//int isNumber;
+	token1 = strtok(positionString, POS_DELIM);
+	// printf("token %s\n", token1);
+	token2 = strtok(NULL, POS_DELIM);
+	// printf("druhy token %s\n", token2);
+	if(token1 == NULL || token2 == NULL)
+	{
+		return 1;
+	}
+	
+	if (isNumber(token1) == FALSE || isNumber(token2) == FALSE)
+	{
+		return 1;
+	}
+	
+
+	
 	return 0;
 }
 //return 1 ak je string cislo, inac 0
